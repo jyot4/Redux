55530afa50ba5ff6a4936f834f39d8a1ef28563b
jyot4 <jyotivyass2000@gmail.com>
2023-09-09 12:46:29 +0530

    latest commit

--- src/todoList/Todo.js
+++ src/todoList/Todo.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setInput , add ,remove} from './TodoSlice'
+import { setInput , add ,remove,update } from './TodoSlice'
+import './Todo.css'
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 
@@ -16,16 +20,19 @@ function Todo() {
 
   return (
     <>
-      <div>
+    <div className='main'>
+      <div className='left'>
         <input type='text' placeholder='enter anything' value={todoselect.inputvalue} onChange={(e) => dispatch(setInput(e.target.value))}></input>
-        <button onClick={() => dispatch(add())}>Print</button>
+        <button onClick={() => dispatch(add())}><AddIcon/></button>
       </div>
+      <div>
       <ul>
         {
           todoselect.list.map((item , index) => {
             return <li>{item} 
 
-            <button onClick={()=>dispatch(remove(index))}>remove</button>
+            <button onClick={()=>dispatch(remove(index))}><DeleteIcon/></button> 
+            <button onClick={()=>dispatch(update(index))}><EditIcon/></button>
             </li>
             
 
@@ -33,7 +40,8 @@ function Todo() {
         }
 
       </ul>
-
+      </div>
+      </div>
 
     </>
   )
--- src/todoList/TodoSlice.js
+++ src/todoList/TodoSlice.js
@@ -6,6 +6,7 @@ export const lists = createSlice({
     initialState: {
         inputvalue: "",
         list: [],
+        updateValue: -1
 
 
     },
@@ -15,18 +16,43 @@ export const lists = createSlice({
             state.inputvalue = action.payload
         },
         add: (state, action) => {
-            state.list = [...state.list, state.inputvalue]
-            state.inputvalue = " "
+            // state.list = [...state.list, state.inputvalue]
+
+            if (state.updateValue !== -1) {
+                const editindex = [...state.list]
+                editindex[state.updateValue] = state.inputvalue
+                state.list = editindex;
+                state.updateValue = -1
+
+                console.log(editindex)
+            }
+            else {
+                if (state.inputvalue.length !== 0) {
+                    state.list = [...state.list, state.inputvalue]
+                }
+
+                state.inputvalue = " "
+            }
         },
         remove: (state, action) => {
-            const handleData =  state.list.filter((any,i)=> i !== action.payload)
-            state.list  =  handleData
-        }
+            const handleData = state.list.filter((any, i) => i !== action.payload)
+            state.list = handleData
+        },
 
+        update: (state, action) => {
+            // setInput(state.list, state.inputvalue)
+            // state.editindex = [...state.inputvalue]
+
+            state.updateValue = action.payload
+                state.inputvalue = state.list[state.updateValue]
+               
+            
+
+        }
     }
 })
 
-export const { add, setInput, remove } = lists.actions;
+export const { add, setInput, remove, update} = lists.actions;
 export default lists.reducer
 
 
